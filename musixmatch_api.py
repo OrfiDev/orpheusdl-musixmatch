c1813662a593475b018fe493213fe23d8588e694
Dniel97 <6324072+Dniel97@users.noreply.github.com>
2022-05-09 17:52:47 +0200

    Beta 1.0
    - Added word-by-word lyrics (Enhanced LRC)
    - Added LyricsX formatting options
    - Hopefully fixed the rate limit by using multiple tokens?
    - Added Captcha message
    - Better user token method added (without API key)

--- musixmatch_api.py
+++ musixmatch_api.py
@@ -1,10 +1,21 @@
-import hmac, base64
-from urllib import parse
+import base64
+import hmac
 from datetime import datetime
 from os import urandom
+from urllib import parse
 from uuid import uuid4
 
 from utils.utils import create_requests_session
+
+
+class CaptchaError(Exception):
+    def __init__(self, message):
+        super(CaptchaError, self).__init__(message)
+
+
+class UserTokenError(Exception):
+    def __init__(self, message):
+        super(UserTokenError, self).__init__(message)
 
 
 class Musixmatch:
@@ -14,17 +25,19 @@ class Musixmatch:
         self.exception = exception
         self.headers = {
             'Connection': 'Keep-Alive',
-            'User-Agent': 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Musixmatch/0.19.4 Chrome/58.0.3029.110 Electron/1.7.6 Safari/537.36'
+            'User-Agent': 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Musixmatch/0.19.4 Chrome/58.0.3029.110 Electron/1.7.6 Safari/537.36 '
         }
         self.user_token = None
-    
+
     def sign_request(self, method, params, timestamp):
         to_hash = self.API_URL + method + '?' + parse.urlencode(params)
-        key = ("IEJ5E8XFaH" "QvIQNfs7IC").encode() # Thanks to https://github.com/aaronlpv/live-lyrics/blob/master/musixmatch.c for the desktop app hmac key
+        # Thanks to https://github.com/aaronlpv/live-lyrics/blob/master/musixmatch.c for the desktop app hmac key
+        key = ("IEJ5E8XFaH" "QvIQNfs7IC").encode()
         signature = hmac.digest(key, (to_hash + timestamp).encode(), digest='SHA1')
         return base64.urlsafe_b64encode(signature).decode()
 
-    def get_user_token(self):
+    def get_user_token_old(self):
         currenttime = datetime.now()
         timestamp = currenttime.strftime('%Y-%m-%dT%H:%M:%SZ')
         signature_timestamp = currenttime.strftime('%Y%m%d')
@@ -41,10 +54,30 @@ class Musixmatch:
         params['signature_protocol'] = 'sha1'
 
         r = self.s.get(self.API_URL + method, params=params, headers=self.headers, cookies={'AWSELB': 'unknown'})
-        if r.status_code != 200: raise Exception(r.text)
-        
+        if r.status_code != 200:
+            raise Exception(r.text)
+
         self.user_token = r.json()['message']['body']['user_token']
-        if self.user_token == 'UpgradeOnlyUpgradeOnlyUpgradeOnlyUpgradeOnly': raise Exception('Musixmatch: getting token failed')
+        if self.user_token == 'UpgradeOnlyUpgradeOnlyUpgradeOnlyUpgradeOnly':
+            raise Exception('Musixmatch: getting token failed')
+        return self.user_token
+
+    def get_user_token(self):
+        r = self.s.get(f'{self.API_URL}token.get', headers=self.headers, params={
+            'user_language': 'en', 'app_id': 'web-desktop-app-v1.0'
+        }, cookies={
+            'AWSELB': '0', 'AWSELBCORS': '0'
+        })
+
+        r = r.json()
+        if r['message']['header']['status_code'] == 401 and r['message']['header']['hint'] == 'captcha':
+            raise CaptchaError('Captcha required')
+        elif r['message']['header']['status_code'] != 200:
+            raise self.exception(f"Error: {r['message']['header']['hint']}")
+
+        self.user_token = r['message']['body']['user_token']
+        if self.user_token == 'UpgradeOnlyUpgradeOnlyUpgradeOnlyUpgradeOnly':
+            raise UserTokenError('Getting user token failed')
         return self.user_token
 
     def _get(self, url: str, query: dict):
@@ -54,25 +87,51 @@ class Musixmatch:
         }
         params.update(query)
 
-        r = self.s.get(f'{self.API_URL}{url}', params=params, headers=self.headers, cookies={'AWSELB': 'unknown'})
+        r = self.s.get(f'{self.API_URL}{url}', params=params, headers=self.headers, cookies={
+            'AWSELB': '0', 'AWSELBCORS': '0'
+        })
         if r.status_code not in [200, 201, 202]:
             raise self.exception(r.text)
+
         r = r.json()
-        if r['message']['header']['status_code'] != 200:
+        if r['message']['header']['status_code'] == 401 and r['message']['header']['hint'] == 'captcha':
+            # throw a captcha error
+            raise CaptchaError('Captcha required')
+        elif r['message']['header']['status_code'] != 200:
             return None
+
         return r['message']['body']
+
+    def get_search_by_track(self, track_name: str, artist_name: str):
+        # needed for richsync?
+        return self._get('track.search', {
+            'q_track': track_name,
+            'q_artist': artist_name,
+            'f_has_lyrics': 1
+        })['track_list']
 
     def get_track_by_isrc(self, isrc: str):
         r = self._get('track.get', {'track_isrc': isrc})
         return r['track'] if r else None
 
+    def get_lyrics_by_id(self, track_id: str):
+        r = self._get('track.lyrics.get', {'track_id': track_id})
+        return r['lyrics'] if r else None
+
     def get_subtitle_by_id(self, common_track_id: str):
         r = self._get('track.subtitle.get', {'commontrack_id': common_track_id})
         return r['subtitle'] if r else None
 
+    def get_rich_sync_by_id(self, track_id: str):
+        # requires track_id and not common_track_id
+        r = self._get('track.richsync.get', {'track_id': track_id})
+        return r['richsync'] if r else None
+
     def get_lyrics_by_metadata(self, track_name: str, artist_name: str):
-        r = self._get('macro.subtitles.get', {'q_artist': artist_name, 'q_track': track_name})
-        if r:
-            subtitle = r['macro_calls']['track.subtitles.get']['message']
-            if subtitle['header']['status_code'] == 200 and subtitle['body']:
-                return r['macro_calls']['track.subtitles.get']['message']['body']['subtitle_list'][0]['subtitle']
+        return self._get('macro.subtitles.get', {
+            'q_artist': artist_name,
+            'q_track': track_name,
+            'format': 'json',
+            'namespace': 'lyrics_richsynched',
+            'optional_calls': 'track.richsync'
+        })['macro_calls']
